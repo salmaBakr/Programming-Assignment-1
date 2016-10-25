d0420d3e614cc907a1d349ad06bd9217b2fa4c9b
Salma Bakr <salmabakr.94@gmail.com>
2016-10-25 03:30:13 +0200

    server socket added

--- server.cpp
+++ server.cpp
@@ -1,121 +1,107 @@
-
 #include <iostream>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <vector>
 using namespace std;
-using namespace std;
+int main(int argc,char *argv[]){
 
-int main()
-{
-
-    int client, server;
+    int fd_client, fd_server;
     int portNum = 1500;
+    if(argc>1){
+        portNum=atoi(argv[1]);
+    }
     bool isExit = false;
     int bufsize = 1024;
     char buffer[bufsize];
+    int maxClients=100;
+    struct sockaddr_in server_addr,client_addr;
+    socklen_t len=sizeof(client_addr);
 
-    struct sockaddr_in server_addr;
-    socklen_t size;
+    fd_server = socket(AF_INET, SOCK_STREAM, 0);
 
-    /* ---------- ESTABLISHING SOCKET CONNECTION ----------*/
-    /* --------------- socket() function ------------------*/
-
-    client = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (client < 0) 
+    if (fd_server < 0)
     {
         cout << "\nError establishing socket..." << endl;
         exit(1);
     }
-
-    cout << "\n=> Socket server has been created..." << endl;
-
+    cout << "success: established socket..." << endl;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htons(INADDR_ANY);
     server_addr.sin_port = htons(portNum);
 
-    /* ---------- BINDING THE SOCKET ---------- */
-    /* ---------------- bind() ---------------- */
-
-
-    if ((bind(client, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0) 
+    if ((bind(fd_server, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0)
     {
         cout << "=> Error binding connection, the socket has already been established..." << endl;
-        return -1;
-    }
-
-  
-    size = sizeof(server_addr);
-    cout << "=> Looking for clients..." << endl;
-
-    /* ------------- LISTENING CALL ------------- */
-    /* ---------------- listen() ---------------- */
-
-    listen(client, 1);
-    int clientCount = 1;
-    server = accept(client,(struct sockaddr *)&server_addr,&size);
-
-    // first check if it is valid or not
-    if (server < 0) 
-        cout << "=> Error on accepting..." << endl;
-
-    while (server > 0) 
-    {
-        strcpy(buffer, "=> Server connected...\n");
-        send(server, buffer, bufsize, 0);
-        cout << "=> Connected with the client #" << clientCount << ", you are good to go..." << endl;
-        cout << "\n=> Enter # to end the connection\n" << endl;
-        cout << "Client: ";
-        do {
-            int n = read(server, buffer, 255);
-            if (n < 0) 
-                 printf("ERROR reading from socket");
-            printf("%s\n", buffer);
-                printf("Please enter the message: ");
-            bzero(buffer,256);
-            strcpy(buffer, "recieved\n");
-           n = write(server, buffer, strlen(buffer));
-            if (n < 0) 
-                printf("ERROR writing to socket");
-            bzero(buffer,256);
-            
-                
-        } while (!isExit);
-
-        /* 
-            Once a connection has been established, both ends 
-            can both read and write to the connection. Naturally, 
-            everything written by the client will be read by the 
-            server, and everything written by the server will be 
-            read by the client.
-        */
-
-        /* ---------------- CLOSE CALL ------------- */
-        /* ----------------- close() --------------- */
-
-        /* 
-            Once the server presses # to end the connection,
-            the loop will break and it will close the server 
-            socket connection and the client connection.
-        */
-
-        // inet_ntoa converts packet data to IP, which was taken from client
-        cout << "\n\n=> Connection terminated with IP " << inet_ntoa(server_addr.sin_addr);
-        close(server);
-        cout << "\nGoodbye..." << endl;
-        isExit = false;
+        close(fd_server);
         exit(1);
     }
+    cout << "success: bind connection..." << endl;
+    if(listen(fd_server, 100)==-1){
+        cout << "=> Error listening..." << endl;
+        close(fd_server);
+        exit(1);
+    }
+    cout << "listening..." << endl;
+    while(true){
+        fd_client= accept(fd_server,(struct sockaddr *)&server_addr,&len);
+        if(fd_client== -1){
+            cout << "error accepting"<<endl;
+            continue;
+        }
+        strcpy(buffer, "=> Server connected...\n");
+        send(fd_client, buffer, bufsize, 0);
+        cout << "success: client connected..." << endl;
+        int pid=fork();
+        if(!pid){
+            cout << "in child process..."<<getpid() << endl;
+            int read_size;
+            if(read_size=recv(fd_client, buffer, bufsize-1, 0)>0){
+                cout <<"=>"<< buffer << endl;
+                vector<string> vec;
+                char * pch;
+                pch = strtok (buffer," ");
+                while (pch != NULL){
+                    vec.push_back(pch);
+                    pch = strtok (NULL, " ");
+                }
+                if(vec[0].compare("GET")==0){
+                    //while not empty line received
+                        //print the optional commands
+                    //try to open file
+                    //if not found
+                        // send 404
+                    //else
+                        // send 200
+                        //read file into buffer and send
+                        //send blank line
 
-    close(client);
-    return 0;
+                }else if(vec[0].compare("POST")==0){
+                    //send 200
+                    //receive all chunks of the file
+                    //save file
+
+                }
+
+            }
+            else if(read_size == 0){
+                cout <<"Client disconnected"<<endl;
+            }
+            else if(read_size == -1){
+               cout <<"recv failed"<<endl;
+            }
+            close(fd_client);
+            close(fd_server);
+            exit(0);
+        }
+        close(fd_client);
+    }
+close(fd_client);
 }
